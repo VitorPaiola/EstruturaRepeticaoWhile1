0c7b96d49224605e8e8bc369628da3f03b36e652
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:43:38 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaRepeticaoWhile1/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+while (n != 2002) {
+    Console.WriteLine("Senha Invalida");
+    n = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Acesso permitido");
